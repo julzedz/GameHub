a5d24fde630e6aee81a553c505296bd97deae988
julzedz <edoziechukwuma@gmail.com>
2024-08-11 17:38:44 +0100

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImage from '../assets/placeholder-image.jpg'
+
 const getCroppedImageUrl =(url: string) => {
-  if (!url) return '';
+  if (!url) return noImage;
   const target = 'media/'
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + 'crop/600/400/' + url.slice(index);

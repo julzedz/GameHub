1bbb4f06913b57031caba8981dec9ce07bfd8eb2
julzedz <edoziechukwuma@gmail.com>
2024-09-06 11:23:11 +0100

    Modify fetch Genre

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -8,11 +8,13 @@ export interface Genre {
   image_background: string;
 }
 
-const useGenres = () => useQuery({
-  queryKey: ['genres'],
-  queryFn: () => 
-    apiClient
-      .get<FetchResponse <Genre>>('/genres').then(res => res.data)
-});
+const useGenres = () =>
+  useQuery({
+    queryKey: ["genres"],
+    queryFn: () =>
+      apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    // initialData: genres,
+  });
 
 export default useGenres;
